9d846cce474c6791676fa78048d93b7fa3ed1a4b
emil.beyerov <emil.beyerov@digigo.work>
2025-06-02 15:24:07 +0400

    Test for Custom Stack and Queue

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,9 @@ package org.example;
 import org.example.list.MyArrayList;
 import org.example.list.MyLinkedList;
 import org.example.list.MyList;
+import org.example.stack.MyStack;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +15,45 @@ public class Main {
         testLinkedList();
         testMyLinkedList();
 
+        testStack();
+        testMyStack();
+
+        testQueue();
+        testMyQueue();
+    }
+
+    private static void testMyQueue() {
+        MyQueue<Integer> queue = new MyQueue<>();
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        System.out.println("Remove first element of myQueue::"+queue.poll());
+        System.out.println("Now the first element::"+queue.peek());
+    }
+
+    private static void testQueue() {
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        System.out.println("Remove first element of queue::"+queue.poll());
+        System.out.println("Now the first element::"+queue.peek());
+    }
+
+    private static void testMyStack() {
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println("Last element of myStack is::"+stack.pop());
+    }
+
+    private static void testStack() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println("Last element of stack is::"+stack.pop());
     }
 
     private static void testMyLinkedList() {
